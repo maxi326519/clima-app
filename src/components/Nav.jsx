3cd10952df21cdf9b9c8de65a648cc75beaab4d4
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-10-30 03:11:42 -0300

    Update styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,17 @@
 import Switch from "./Switch.jsx";
 import SearchBar from "./SearchBar.jsx";
 
+import icon from "../assets/svg/icon.svg";
 import "./styles/Nav.scss";
 
 export default function Nav({ darkMode, handleThemeChange, onSearch }) {
   return (
     <nav>
       <div className="nav width-container">
-        <h1 className="title">Weather App</h1>
+        <div className="nav-logo">
+          <img className="icon" src={icon} />
+          <h1 className="title">Weather App</h1>
+        </div>
         <Switch darkMode={darkMode} handleThemeChange={handleThemeChange} />
         <SearchBar darkMode={darkMode} onSearch={onSearch} />
       </div>
