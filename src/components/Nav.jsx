30a34af00b3e66e3e83302fd3cb1c41da0da2d8a
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-10-31 16:32:12 -0300

    Update mobile nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { getCity } from '../redux/actions';
 
@@ -5,21 +6,37 @@ import Switch from "./Switch.jsx";
 import SearchBar from "./SearchBar.jsx";
 
 import icon from "../assets/svg/icon.svg";
+import bars from "../assets/svg/bars.svg";
+import arrow from "../assets/svg/arrow.svg";
 import "./styles/Nav.scss";
 
 export default function Nav({ darkMode, handleThemeChange }) {
 
   const dispatch = useDispatch();
+  const [ options, setOptions ] = useState({ isActive: false, class: '', icon: bars});
+
+  function handleOptions(){
+    console.log('cahnge');
+    if(options.isActive)
+      setOptions({ isActive: false, class: '', icon: bars});
+    else
+      setOptions({ isActive: true, class: 'menu', icon: arrow});
+  } 
 
   return (
     <nav>
-      <div className="nav width-container">
-        <div className="nav-panel">
-          <img className="icon" src={icon} alt='icon'/>
-          <h1 className="title">Weather App</h1>
+      <div className={`nav nav${ darkMode.class } width-container`}>
+        <div className={`nav-panel ${ options.class }`}>
+          <div className='logo'>
+            <img className="icon" src={icon} alt='icon'/>
+            <h1 className="title">Weather App</h1>
+          </div>
+          <span>Home</span>
+          <span>About me</span>
           <Switch darkMode={darkMode} handleThemeChange={handleThemeChange} />
         </div>
         <SearchBar darkMode={darkMode} getCity={ city => dispatch(getCity(city)) } />
+        <img className="icon options" src={ options.icon } alt='bars' onClick={ handleOptions }/>
       </div>
     </nav>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
-import './styles/SearchBar.scss'
+import React, { useState } from "react";
+import "./styles/SearchBar.scss";
+
+import search from "../assets/svg/search.svg";
 
 export default function SearchBar({ darkMode, getCity }) {
-
   const [city, setCity] = useState("");
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
     getCity(city);
   }
-  
+
   return (
-    <form className={`search-bar search-bar${ darkMode.class }`} onSubmit={handleSubmit}>
-      <input
-        type='text'
-        placeholder='City...'
-        onChange={e => setCity(e.target.value)}
-      />
-      <button className='sear-bar__btn' type='submit'>Search</button>
+    <form
+      className={`search-bar search-bar${darkMode.class}`}
+      onSubmit={handleSubmit}
+    >
+      <img className="icon" src={search} alt="" />
+      <div className="emergente">
+        <input
+          type="text"
+          placeholder="City..."
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button className="sear-bar__btn" type="submit">
+          Search
+        </button>
+      </div>
     </form>
-  )
-};
+  );
+}
